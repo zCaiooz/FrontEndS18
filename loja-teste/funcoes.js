94becf156af4f4583bcb4b1d5358d6e995596ec7
zCaiooz <00001107103629sp@al.educacao.sp.gov.br>
2026-09-04 21:03:36 -0300

    em busca da sanidade

--- loja-teste/funcoes.js
+++ loja-teste/funcoes.js
@@ -2,7 +2,6 @@ function calcularTotal(preco, quantidade) {
     return preco * quantidade;
 }
 
-// Permite usar a função nos testes com Jest
 if (typeof module !== "undefined") {
     module.exports = { calcularTotal };
 }
